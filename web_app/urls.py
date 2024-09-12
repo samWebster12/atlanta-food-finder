029f9b2bcd1a35cb7cd81ca3debccdcc370377d8
samWesbter12 <samuelalexwebster@gmail.com>
2024-09-12 15:13:54 -0400

    added google maps functionality

--- web_app/urls.py
+++ web_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search_restaurants, name='search_restaurants'),
 ]
--- web_app/views.py
+++ web_app/views.py
@@ -1,4 +1,35 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
 
 def index(request):
     return render(request, 'index.html')
+
+
+def search_restaurants(request):
+    print("receievd request")
+    query = request.GET.get('query', '')
+    # You'll need to set up your API key in your Django settings
+    api_key = 'AIzaSyDRbtKq5nh6cpCD_HVe09TqO7nuZEttfUk'
+
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    params = {
+        'location': '33.7490,-84.3880',  # Coordinates for Atlanta
+        'radius': '5000',  # Search within 5km
+        'type': 'restaurant',
+        'keyword': query,
+        'key': api_key
+    }
+    
+    response = requests.get(url, params=params)
+    results = response.json().get('results', [])
+    
+    restaurants = []
+    for result in results:
+        restaurants.append({
+            'name': result['name'],
+            'lat': result['geometry']['location']['lat'],
+            'lng': result['geometry']['location']['lng'],
+        })
+    
+    return JsonResponse({'restaurants': restaurants})
