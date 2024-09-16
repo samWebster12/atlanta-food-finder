ac8351940e30d42e7f1f2ea8d029914fde305cf4
mfaustino141 <maggie.faustino141@gmail.com>
2024-09-16 17:38:22 -0400

    Made the comment accurate
    The comment next to the radius still said 5 km so I changed it to 15 miles because that's what it is

--- web_app/views.py
+++ web_app/views.py
@@ -15,7 +15,7 @@ def search_restaurants(request):
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
     params = {
         'location': '33.7490,-84.3880',  # Coordinates for Atlanta
-        'radius': '24140.16',  # Search within 5km
+        'radius': '24140.16',  # Search within 15 miles
         'type': 'restaurant',
         'keyword': query,
         'key': api_key
